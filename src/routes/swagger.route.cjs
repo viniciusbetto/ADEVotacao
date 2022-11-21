893702514d035f35d8b563c280cbb81859081d0b
viniciusbetto@gmail.com <viniciusbetto@gmail.com>
2022-11-21 19:07:51 -0300

    Renomear route swagger com extensão cjs

--- src/routes/swagger.route.cjs
+++ src/routes/swagger.route.cjs
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../swagger.json" assert { type: "json" };
+import swaggerDocument from "../swagger.json"; //assert { type: "json" };
 
 export const routeSwagger = Router();
 
